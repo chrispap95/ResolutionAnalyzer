54caf22b0fca9e65e160d92a6b22b7cdffb41e88
Christos Papageorgakis <christos.papageorgakis@cern.ch>
2019-12-16 13:07:55 -0500

    Fix new lines issue

--- scripts/cfgMaker.sh
+++ scripts/cfgMaker.sh
@@ -2,7 +2,5 @@
 
 for i in 5 10 15 20 30 40 60 80 100
 do
-    echo "/eos/uscms/store/user/chpapage/SingleGamma_E${i}Eta1p7/SingleGamma_E${i}Eta1p7_CMSSW_10_6_3_patch1_upgrade2023_D41_ntuples/"
-    echo `ls /eos/uscms/store/user/chpapage/SingleGamma_E${i}Eta1p7/SingleGamma_E${i}Eta1p7_CMSSW_10_6_3_patch1_upgrade2023_D41_ntuples/`
-    echo "/0000/"
+    echo "/eos/uscms/store/user/chpapage/SingleGamma_E${i}Eta1p7/SingleGamma_E${i}Eta1p7_CMSSW_10_6_3_patch1_upgrade2023_D41_ntuples/"`ls /eos/uscms/store/user/chpapage/SingleGamma_E${i}Eta1p7/SingleGamma_E${i}Eta1p7_CMSSW_10_6_3_patch1_upgrade2023_D41_ntuples/`"/0000/"
 done
