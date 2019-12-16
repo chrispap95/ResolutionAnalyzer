15638c99d0d0e036dd2d5a0e3c3665dc25094ea0
Christos Papageorgakis <christos.papageorgakis@cern.ch>
2019-12-16 14:10:49 -0500

    Debugging cfgMaker

--- scripts/cfgMaker.sh
+++ scripts/cfgMaker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-for df in "01" "03" "05" "07"
+for df in 01 03 05 07
 for i in 5 10 15 20 30 40 60 80 100
 do
 echo "outFilePath = output/out_E${i}Eta1p7_df${df}.root" > simpleBH_E${i}Eta1p7_df${df}.cfg
