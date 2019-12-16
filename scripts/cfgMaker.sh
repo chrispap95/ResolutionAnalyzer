a51d6c24b76180a09b706f4f3fdfc73ee305b42a
Christos Papageorgakis <christos.papageorgakis@cern.ch>
2019-12-16 13:54:46 -0500

    Debugging cfgMaker

--- scripts/cfgMaker.sh
+++ scripts/cfgMaker.sh
@@ -2,9 +2,7 @@
 
 for i in 5 10 15 20 30 40 60 80 100
 do
-cat > simpleBH_E${i}Eta1p7.cfg << "EOF"
-outFilePath = output/out_E${i}Eta1p7.root
-EOF
+echo "outFilePath = output/out_E${i}Eta1p7.root" > simpleBH_E${i}Eta1p7.cfg
 echo "filePath = root://cmseos.fnal.gov//store/user/chpapage/SingleGamma_E${i}Eta1p7/SingleGamma_E${i}Eta1p7_CMSSW_10_6_3_patch1_upgrade2023_D41_ntuples/"`ls /eos/uscms/store/user/\
 chpapage/SingleGamma_E${i}Eta1p7/SingleGamma_E${i}Eta1p7_CMSSW_10_6_3_patch1_upgrade2023_D41_ntuples/`"/0000" >> simpleBH_E${i}Eta1p7.cfg
 cat >> simpleBH_E${i}Eta1p7.cfg << "EOF"
