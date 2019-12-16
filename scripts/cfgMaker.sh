106476b3f8ee4d05d9a3662abde896a008a996db
Christos Papageorgakis <christos.papageorgakis@cern.ch>
2019-12-16 13:06:31 -0500

    cfgMaker can now print the directory of the ntuples

--- scripts/cfgMaker.sh
+++ scripts/cfgMaker.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/sh
 
 for i in 5 10 15 20 30 40 60 80 100
-    do ls /eos/uscms/store/user/chpapage/SingleGamma_E${i}Eta1p7/SingleGamma_E${i}Eta1p7_CMSSW_10_6_3_patch1_upgrade2023_D41_ntuples/
+do
+    echo "/eos/uscms/store/user/chpapage/SingleGamma_E${i}Eta1p7/SingleGamma_E${i}Eta1p7_CMSSW_10_6_3_patch1_upgrade2023_D41_ntuples/"
+    echo `ls /eos/uscms/store/user/chpapage/SingleGamma_E${i}Eta1p7/SingleGamma_E${i}Eta1p7_CMSSW_10_6_3_patch1_upgrade2023_D41_ntuples/`
+    echo "/0000/"
 done
