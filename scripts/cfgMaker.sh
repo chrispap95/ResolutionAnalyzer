0581f2a7e33bbea1e6b2c10fff090a476fe76b48
Christos Papageorgakis <christos.papageorgakis@cern.ch>
2019-12-16 13:02:30 -0500

    Update ls command

--- scripts/cfgMaker.sh
+++ scripts/cfgMaker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 
 for i in 5 10 15 20 30 40 60 80 100
-    do eosls /store/user/chpapage/SingleGamma_E${i}Eta1p7/SingleGamma_E${i}Eta1p7_CMSSW_10_6_3_patch1_upgrade2023_D41_ntuples/
+    do ls eos/uscms/store/user/chpapage/SingleGamma_E${i}Eta1p7/SingleGamma_E${i}Eta1p7_CMSSW_10_6_3_patch1_upgrade2023_D41_ntuples/
 done
