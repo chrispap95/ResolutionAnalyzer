f56b270fc4d8625fd001c1ee0d5bfe99c93a37f9
Christos Papageorgakis <christos.papageorgakis@cern.ch>
2019-12-16 13:53:02 -0500

    Debugging cfgMaker

--- scripts/cfgMaker.sh
+++ scripts/cfgMaker.sh
@@ -4,8 +4,10 @@ for i in 5 10 15 20 30 40 60 80 100
 do
 cat > simpleBH_E${i}Eta1p7.cfg << "EOF"
 outFilePath = output/out_E${i}Eta1p7.root
-filePath = root://cmseos.fnal.gov//store/user/chpapage/SingleGamma_E${i}Eta1p7/SingleGamma_E${i}Eta1p7_CMSSW_10_6_3_patch1_upgrade2023_D41_ntuples/`ls /eos/uscms/store/user/\
-chpapage/SingleGamma_E${i}Eta1p7/SingleGamma_E${i}Eta1p7_CMSSW_10_6_3_patch1_upgrade2023_D41_ntuples/`/0000
+EOF
+echo "filePath = root://cmseos.fnal.gov//store/user/chpapage/SingleGamma_E${i}Eta1p7/SingleGamma_E${i}Eta1p7_CMSSW_10_6_3_patch1_upgrade2023_D41_ntuples/"`ls /eos/uscms/store/user/\
+chpapage/SingleGamma_E${i}Eta1p7/SingleGamma_E${i}Eta1p7_CMSSW_10_6_3_patch1_upgrade2023_D41_ntuples/`"/0000" >> simpleBH_E${i}Eta1p7.cfg
+cat >> simpleBH_E${i}Eta1p7.cfg << "EOF"
 recoFileName = ntuples
 nRuns = 80
 deadfrac = 0.1
