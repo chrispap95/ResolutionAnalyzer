68b282e78b13170eef8b224afe36635008b5b998
Christos Papageorgakis <chrispap95@gmail.com>
2020-01-28 14:01:57 -0600

    Merged preparation files for condor submission.

--- prepareCondor.sh
+++ prepareCondor.sh
@@ -2,6 +2,6 @@
 USERBASE=`pwd`
 cd ../../../
 tar --exclude="*.root" --exclude-vcs -zcvf CMSSW_10_6_3_patch1.tgz CMSSW_10_6_3_patch1/
-xrdcp -f CMSSW_10_6_3_patch1.tgz root://cmseos.fnal.gov//store/user/chpapage/CMSSW_10_6_3_patch1.tgz
-rm -f CMSSW_10_6_3_patch1.tgz
+# xrdcp -f CMSSW_10_6_3_patch1.tgz root://cmseos.fnal.gov//store/user/chpapage/CMSSW_10_6_3_patch1.tgz
+mv CMSSW_10_6_3_patch1.tgz CMSSW_10_6_3_patch1/src/ResolutionAnalyzer
 cd $USERBASE
--- prepareCondor_new.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-USERBASE=`pwd`
-cd ../../../
-tar --exclude="*.root" --exclude-vcs -zcvf CMSSW_10_6_3_patch1.tgz CMSSW_10_6_3_patch1/
-# xrdcp -f CMSSW_10_6_3_patch1.tgz root://cmseos.fnal.gov//store/user/chpapage/CMSSW_10_6_3_patch1.tgz
-mv CMSSW_10_6_3_patch1.tgz CMSSW_10_6_3_patch1/src/ResolutionAnalyzer
-cd $USERBASE
