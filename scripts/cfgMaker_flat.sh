f871d3c509befc374839f247ead1bf04e0a7d775
Christos Papageorgakis <Christos.Papageorgakis@cern.ch>
2020-10-21 21:20:50 -0400

    Scripts can now split jobs

--- scripts/cfgMaker_flat.sh
+++ scripts/cfgMaker_flat.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/sh
 
-# Basic configuration values - self explanatory
-
+# Basic configuration values - most are self explanatory
+#     - filesToProcess: number of files to use for each dead fraction.
+#     - splitLevel: maximum number of files per job.
+#                   For no splitting, set it >= ${deadFractions[0]}
 energyRange=0to3000
 eta=1p7
 pGenerator=SingleGamma
@@ -10,23 +12,71 @@ geometry=upgrade2023_D41
 siteUrl=root://cmseos.fnal.gov/
 deadFractions=(01 03 05 07)
 filesToProcess=(298 99 60 43)
+splitLevel=300
 firstFile=0
+
+# Checks if any dead fraction has been split
+function isSplit() {
+  for i in $@
+  do
+    if [ $i -ne 1 ]
+    then
+      echo 1
+      exit
+    fi
+  done
+  echo 0
+}
+
+# Prepare arrays for job splitting
 configuration=${pGenerator}_E${energyRange}Eta${eta}
+numberOfJobs=()
+lastJobFiles=()
+for files in ${filesToProcess[@]}
+do
+  if [ $((${files}%${splitLevel})) -eq 0 ]
+  then
+    numberOfJobs+=($((${files}/${splitLevel})))
+    lastJobFiles+=(${splitLevel})
+  else
+    numberOfJobs+=($((${files}/${splitLevel} + 1)))
+    lastJobFiles+=($((${files}%${splitLevel})))
+  fi
+done
 
-# Loop over dead fractions and energies
+# Print details about splitting
+echo "Jobs are split as: (${numberOfJobs[@]})"
+echo "Copy and paste this list to condorSubmitter_flat.sh"
+
+# Loop over dead fractions and jobs per dead fraction
 # nRuns: number of files to process
-
 i=0
+k=0
 for df in ${deadFractions[@]}
 do
-namestring=E${energyRange}Eta${eta}_df${df}
-samplesPath=store/user/${USER}/${configuration}/${configuration}_${cmssw}_${geometry}_ntuples/
-echo "outFilePath = out_${namestring}.root" > simpleBH_${namestring}.cfg
-echo "filePath = ${siteUrl}/${samplesPath}"`ls /eos/uscms/${samplesPath}`"/0000" >> simpleBH_${namestring}.cfg
-echo "recoFileName = ntuples" >> simpleBH_${namestring}.cfg
-echo "nRuns = ${filesToProcess[$i]}" >> simpleBH_${namestring}.cfg
-echo "deadfrac = 0.${df}" >> simpleBH_${namestring}.cfg
-echo "firstRun = ${firstFile}" >> simpleBH_${namestring}.cfg
-firstFile=$(($firstFile + ${filesToProcess[$i]}))
-((i+=1))
+  for j in `seq ${numberOfJobs[${k}]}`
+  do
+    if [ $(isSplit "${numberOfJobs[@]}") -eq 1 ]
+    then
+      namestring=E${energyRange}Eta${eta}_df${df}_${j}
+    else
+      namestring=E${energyRange}Eta${eta}_df${df}
+    fi
+    samplesPath=store/user/${USER}/${configuration}/${configuration}_${cmssw}_${geometry}_ntuples/
+    echo "outFilePath = out_${namestring}.root" > simpleBH_${namestring}.cfg
+    echo "filePath = ${siteUrl}/${samplesPath}"`ls /eos/uscms/${samplesPath}`"/0000" >> simpleBH_${namestring}.cfg
+    echo "recoFileName = ntuples" >> simpleBH_${namestring}.cfg
+    echo "deadfrac = 0.${df}" >> simpleBH_${namestring}.cfg
+    echo "firstRun = ${firstFile}" >> simpleBH_${namestring}.cfg
+    if [ ${j} -eq ${numberOfJobs[${k}]} ]
+    then
+      echo "nRuns = ${lastJobFiles[${k}]}" >> simpleBH_${namestring}.cfg
+      firstFile=$((${firstFile} + ${lastJobFiles[${k}]}))
+    else
+      echo "nRuns = ${splitLevel}" >> simpleBH_${namestring}.cfg
+      firstFile=$((${firstFile} + ${splitLevel}))
+    fi
+    ((i+=1))
+  done
+  ((k+=1))
 done
